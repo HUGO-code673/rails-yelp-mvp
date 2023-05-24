e334c5c35dd6d2ec4c58752206acbb321d3ec6cd
hugo guillaumet <senka6@hotmail.com>
2023-05-24 17:05:17 +0200

    dd

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,38 @@
+class RestaurantsController < ApplicationController
+  # before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+  # def index
+  #   if params[:category].blank?
+  #     @restaurants = Restaurant
+  #   else
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+  @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+    redirect_to @restaurant, notice: "Restaurant was successfully created."
+    else
+    render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  # def set_restaurant
+  #   @restaurant = Restaurant.find(params[:id])
+  # end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,13 @@
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+Restaurant.create! = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", phone_number: "1234567895", category: "chinese"}
+Restaurant.create! = {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "1234567821", category: "japanese"}
+Restaurant.create! = {name: "tme_string", address: "chez oit", phone_number: "3615", category: "french"}
+Restaurant.create! = {name: "mama", address: "chez oim", phone_number: "361544", category: "belgian"}
+Restaurant.create! = {name: "mia", address: "chez mia", phone_number: "3611111111", category: "belgian"}
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
